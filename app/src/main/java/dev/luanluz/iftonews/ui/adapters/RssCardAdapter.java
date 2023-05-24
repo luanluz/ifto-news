a100533a43bbca1c8c62a7159be4423bf0a30eb7
Luan Luz <40572816+luanluz@users.noreply.github.com>
2023-05-24 09:52:58 -0300

    ✨ Insere estrutura para consumir RSS

--- app/src/main/java/dev/luanluz/iftonews/ui/adapters/RssCardAdapter.java
+++ app/src/main/java/dev/luanluz/iftonews/ui/adapters/RssCardAdapter.java
@@ -1,6 +1,8 @@
 package dev.luanluz.iftonews.ui.adapters;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +16,17 @@ import java.util.List;
 
 import dev.luanluz.iftonews.R;
 import dev.luanluz.iftonews.model.RssItem;
+import dev.luanluz.iftonews.ui.views.WebViewActivity;
 
 public class RssCardAdapter extends RecyclerView.Adapter<RssCardAdapter.viewHolder> {
+    private Context context;
     private List<RssItem> rssItems;
 
-    public RssCardAdapter(List<RssItem> rssItems) {
+    public RssCardAdapter(
+        Context context,
+        List<RssItem> rssItems
+    ) {
+        this.context = context;
         this.rssItems = rssItems;
     }
 
@@ -57,7 +65,12 @@ public class RssCardAdapter extends RecyclerView.Adapter<RssCardAdapter.viewHold
         holder.rssDescription.setText(rssItem.getDescription());
         holder.rssPubDate.setText(rssItem.getPubDateInRelativeTime());
 
-        holder.itemView.setOnClickListener(view -> Toast.makeText(view.getContext(), rssItem.getTitle(), Toast.LENGTH_SHORT).show());
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, WebViewActivity.class);
+
+            intent.putExtra("link", rssItem.getLink());
+            view.getContext().startActivity(intent);
+        });
     }
 
     @Override
